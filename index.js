5761773634ce46f708ecef3ce7104ae349108120
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-12 19:59:18 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(specified = "roller-skate") {
+
+    return `This Saturday, I want to ${specified}!`
+}
+
+const mondayWork = function (argument = "go to the office") {
+
+   return `This Monday, I will ${argument}.` 
+}
+
+
+let wrapAdjective = function (vf="*") {
+    return function (par ="special") {
+        return `You are ${vf}${par}${vf}!`;
+    } 
+}
+
+console.log(wrapAdjective()())
